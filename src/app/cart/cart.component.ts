c42dc31b8e55dc4fd5c900bc1f3ebde00f771b6c
dnguyen000 <don.nguyen.000@gmail.com>
2023-12-12 14:06:41 -0600

    updated styling for cart component; removed unused methods and comments

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,12 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CartService} from "../services/cart.service";
 import { Product } from "../models/product";
-import { Observable } from "rxjs";
-import {State} from "../models/state";
-import {FormGroup, NgForm} from "@angular/forms";
-import {MatDialog, MatDialogRef} from "@angular/material/dialog";
-import { DialogPopupComponent } from "../dialog-popup/dialog-popup.component";
-import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-cart',
@@ -15,74 +9,17 @@ import {HttpClient} from "@angular/common/http";
 })
 export class CartComponent implements OnInit {
   @Input() products: Product[];
-  public isValid: boolean | undefined;
-  public saveUnsuccessful: boolean = false;
-  dialogRef: MatDialogRef<DialogPopupComponent>;
   subtotal: number = 0;
 
-  states: State[] = [];
-  selectedState: State | undefined;
-  lastName: String;
-  address: String;
-  state: String;
-
-  constructor(private cartService: CartService, public dialog: MatDialog, private httpClient: HttpClient) {
-    this.lastName = "";
-    this.address = "";
-    this.state = "";
-  }
-
-  openDialogSuccess(): void {
-    this.dialogRef = this.dialog.open(DialogPopupComponent, {
-      disableClose: false
-    });
-
-    this.dialogRef.componentInstance.wasSuccessful = "Success!"
-    this.dialogRef.componentInstance.message = "This is a test showing it passed";
-  }
-
-  openDialogFailed(): void {
-    this.dialogRef = this.dialog.open(DialogPopupComponent, {
-      disableClose: false
-    });
-
-    this.dialogRef.componentInstance.wasSuccessful = "Failed!"
-    this.dialogRef.componentInstance.message = "This is a test showing it failed...";
-  }
-
-  closeDialog(): void {
-    this.dialogRef.close();
-  }
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
     this.products = this.cartService.getProducts();
     this.calculateSubtotal();
 
-    this.fetchStates().subscribe(data => {
-      this.states = data;
-    });
-
     this.cartService.clearProducts.subscribe((data: Product[]) => {
       this.products = data;
-      // window.location.reload();
     });
-  }
-
-  fetchStates(): Observable<State[]> {
-    return this.httpClient.get<State[]>("http://localhost:8080/cart")
-  }
-
-  onFormSubmit(ngForm: NgForm) {
-    this.saveUnsuccessful = false;
-    if(!ngForm.valid) {
-      this.saveUnsuccessful = true;
-      this.openDialogFailed();
-      return;
-    }
-    console.log(ngForm);
-    this.openDialogSuccess();
-
-    ngForm.resetForm();
   }
 
   removeProduct(id: number) {
@@ -97,60 +34,5 @@ export class CartComponent implements OnInit {
 
   public set cartSubtotal(subtotal: number) {
     this.cartService.setSubtotal(subtotal);
-  }
-
-  //Shipping
-  public set userFirstName(firstName: string) {
-    this.cartService.setFirstName(firstName);
-  }
-
-  public set userLastName(lastName: string) {
-    this.cartService.setLastName(lastName);
-  }
-
-  public set userAddress(address: string) {
-    this.cartService.setAddress(address);
-  }
-
-  public set userState(state: string) {
-    this.cartService.setState(state);
-  }
-
-  public set userZipCode(address: string) {
-    this.cartService.setZipCode(address);
-  }
-
-  public set userCity(address: string) {
-    this.cartService.setCity(address);
-  }
-
-  public set userPhoneNumber(address: string) {
-    this.cartService.setPhoneNumber(address);
-  }
-
-  //Payment
-  public set userCreditCard(creditCardNumber: string) {
-    this.cartService.setCreditCardNumber(creditCardNumber);
-  }
-
-  public set userCCExpiration(expirationDate: string) {
-    this.cartService.setCCExpiration(expirationDate);
-  }
-
-  public set userCCCVV(creditCardCVV: string) {
-    this.cartService.setCCCVV(creditCardCVV);
-  }
-
-  public validateForms() {
-    this.isValid = this.cartService.validateUserFormInput();
-  }
-
-  public getOrderMessage(): string {
-    return this.isValid ? "Purchased successfully!" : "Error! Please correct highlighted fields.";
-  }
-
-  public getFirstName() {
-    alert(`user shipping info: ${this.cartService.getFirstName()} ${this.cartService.getLastName()}\n${this.cartService.getCity()}, ${this.cartService.getState()} ${this.cartService.getZipCode()}\n${this.cartService.getPhoneNumber()}`);
-    alert(`user credit card info:\ncc number: ${this.cartService.getCCNumber()}\ncc exp: ${this.cartService.getCCExp()}\ncc cvv: ${this.cartService.getCCCVV()}`);
   }
 }
--- src/app/cart/checkout/checkout.component.ts
+++ src/app/cart/checkout/checkout.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import {MatDialog, MatDialogRef} from "@angular/material/dialog";
-import {DialogPopupComponent} from "../../dialog-popup/dialog-popup.component";
-import {FormGroup, NgForm} from "@angular/forms";
-import {State} from "../../models/state";
-import {CartService} from "../../services/cart.service";
+import { MatDialog, MatDialogRef } from "@angular/material/dialog";
+import { DialogPopupComponent } from "../../dialog-popup/dialog-popup.component";
+import { NgForm } from "@angular/forms";
+import { State } from "../../models/state";
+import { CartService } from "../../services/cart.service";
 import { HttpClient, HttpResponse } from "@angular/common/http";
-import {Observable} from "rxjs";
-import {OrderConfirmation} from "../../models/OrderConfirmation";
-import {Success} from "../../models/Success";
+import { Observable } from "rxjs";
+import { OrderConfirmation } from "../../models/OrderConfirmation";
+import { Success } from "../../models/Success";
 
 @Component({
   selector: 'app-checkout',
@@ -19,16 +19,14 @@ export class CheckoutComponent {
   public saveUnsuccessful: boolean = false;
   dialogRef: MatDialogRef<DialogPopupComponent>;
   subtotal: number = 0;
-  reactiveForm: FormGroup;
 
   states: State[] = [];
-  constructor(private cartService: CartService, public dialog: MatDialog, private httpClient: HttpClient) {
-    this.reactiveForm = new FormGroup({
-    });
-  }
+  constructor(private cartService: CartService, public dialog: MatDialog, private httpClient: HttpClient) {}
 
   openDialogSuccess(): void {
     this.dialogRef = this.dialog.open(DialogPopupComponent, {
+      height: '200px',
+      width: '400px',
       disableClose: false
     });
 
@@ -36,12 +34,13 @@ export class CheckoutComponent {
       this.cartService.clearCart();
     });
 
-    this.dialogRef.componentInstance.wasSuccessful = "Success!"
-    this.dialogRef.componentInstance.message = "This is a test showing it passed";
+    this.dialogRef.componentInstance.wasSuccessful = "Order Processed Successfully!"
   }
 
   openDialogFailed(message: string): void {
     this.dialogRef = this.dialog.open(DialogPopupComponent, {
+      height: '200px',
+      width: '400px',
       disableClose: false
     });
 
@@ -101,26 +100,6 @@ export class CheckoutComponent {
     this.cartService.setCCCVV(creditCardCVV);
   }
 
-  private getFormInput(): object {
-    return {
-      "address": {
-        "firstName": this.cartService.getFirstName(),
-        "lastName": this.cartService.getLastName(),
-        "street": this.cartService.getAddress(),
-        "zipCode": this.cartService.getZipCode(),
-        "city": this.cartService.getCity(),
-        "state": this.cartService.getState(),
-        "phoneNumber": this.cartService.getPhoneNumber()
-      },
-      "payment": {
-        "ccNumber": this.cartService.getCCNumber(),
-        "ccExpr": this.cartService.getCCExp(),
-        "ccCVV": this.cartService.getCCCVV()
-      },
-      "products": this.cartService.getProducts()
-    }
-  }
-
   postOrderConfirmation(payload: OrderConfirmation): Observable<HttpResponse<Success>> {
     return this.httpClient.post<Success>("http://localhost:8080/cart/purchase", payload,  {observe: "response"});
   }
@@ -128,8 +107,7 @@ export class CheckoutComponent {
   onFormSubmit(ngForm: NgForm) {
     this.saveUnsuccessful = false;
     this.postOrderConfirmation(this.cartService.getOrderConfirmation()).subscribe({
-      next: response => {
-        console.log(response);
+      next: () => {
         this.openDialogSuccess();
         ngForm.resetForm();
       },
